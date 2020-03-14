d926f3325a6b560d3245e71942cca96736470a50
kanatakka <takashi.kngw@gmail.com>
2020-03-14 22:44:40 +0900

    Import Spring REST Project.

---
+++ src/main/java/com/takanaro/api/takaapi/TakaApiApplication.java
@@ -0,0 +1,13 @@
+package com.takanaro.api.takaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/takanaro/api/takaapi/TakaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takanaro.api.takaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
